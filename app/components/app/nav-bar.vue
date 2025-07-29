be8d5a3ccf6926c33c180e81c75e1faf13930867
Noah Trần <trananhquan1009@gmail.com>
2025-07-30 00:07:00 +0700

    30/07/25 - add landing page (#50)

---
+++ app/components/app/nav-bar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost text-xl">
+        Helium
+      </NuxtLink>
+    </div>
+    <div class="navbar-end">
+      <button class="btn btn-accent">
+        Đăng Nhập Với Google
+        <Icon name="tabler:brand-google-filled" size="24" />
+      </button>
+    </div>
+  </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hero bg-base-300 container mx-auto mt-4">
+    <div class="hero-content text-center min-h-96">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Helium Travel
+        </h1>
+        <p class="py-6">
+          Theo dõi hành trình và cuộc phiêu lưu của bạn với ứng dụng nhật ký du lịch đơn giản này. Thêm địa điểm, ảnh và ghi chú để tạo nhật ký kỹ thuật số về hành trình của bạn.
+        </p>
+        <button class="btn btn-accent">
+          Đăng Nhập Với Google
+          <Icon name="tabler:brand-google-filled" size="24" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
