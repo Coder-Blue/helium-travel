5f530b6b4450bccbf838b9ab6cef05b3598d1574
Coder-Blue <trananhquan1009@gmail.com>
2025-07-30 00:04:40 +0700

    30/07/25 - add landing page

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-blue-500 text-3xl">
-      Hello World
-    </h1>
-    <button class="btn btn-secondary">
-      View Map <Icon name="tabler:map" size="32" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
