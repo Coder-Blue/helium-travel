e37fe6756bb37c8e3a4a4d3a517d0fc91bce0a93
Coder-Blue <trananhquan1009@gmail.com>
2025-07-29 22:46:19 +0700

    29/07/25 - setup tailwindcss / daisy ui / github actions / icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,28 @@
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
-  devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
+  devtools: { enabled: false },
+
+  modules: ["@nuxt/eslint", "@nuxtjs/google-fonts", "@nuxt/icon"],
+
+  css: ["~/assets/css/app.css"],
+
+  googleFonts: {
+    families: {
+      Manrope: {
+        wght: [200, 300, 400, 500, 600, 700, 800],
+      },
+    },
+    display: "swap",
+  },
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
   eslint: {
     config: {
       standalone: false,
