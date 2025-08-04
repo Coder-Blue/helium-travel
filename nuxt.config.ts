678d9bcc0ca2ea958197ef5b6b7242145343b227
Noah Trần <trananhquan1009@gmail.com>
2025-08-04 19:27:03 +0700

    04/08/25 - setup tailwindcss / daisyui / icons (#49)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,28 @@
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
-  modules: ["@nuxt/eslint"],
+  css: ["~/assets/css/app.css"],
+
+  modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/icon"],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 
   eslint: {
     config: { standalone: false },
+  },
+
+  fonts: {
+    defaults: {
+      weights: [400, 700],
+      styles: ["normal", "italic"],
+      subsets: ["latin", "vietnamese"],
+    },
   },
 });
