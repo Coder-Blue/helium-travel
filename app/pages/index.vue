8455ed9531e77f7bd125660f4502708efd8644b2
Noah Trần <trananhquan1009@gmail.com>
2025-08-05 15:12:55 +0700

    05/08/25 - add auth middleware (#57)

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,7 @@
+<script lang="ts" setup>
+const authStore = useAuthStore();
+</script>
+
 <template>
   <div class="hero bg-base-300 container mx-auto mt-4">
     <div class="hero-content text-center min-h-96">
@@ -8,7 +12,14 @@
         <p class="py-6">
           Theo dõi hành trình và cuộc phiêu lưu của bạn với ứng dụng nhật ký du lịch đơn giản này. Thêm địa điểm, ảnh và ghi chú để tạo nhật ký kỹ thuật số về hành trình của bạn.
         </p>
-        <AuthButton />
+        <AuthButton v-if="!authStore.user" />
+        <NuxtLink
+          v-else
+          to="/dashboard"
+          class="btn btn-primary"
+        >
+          Bắt Đầu Du Lịch
+        </NuxtLink>
       </div>
     </div>
   </div>
