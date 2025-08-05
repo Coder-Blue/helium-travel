d730527da24e78f0c058242880f345379c3dd996
Coder-Blue <trananhquan1009@gmail.com>
2025-08-05 14:54:09 +0700

    05/08/25 - add auth state / sign out | fix libsql

--- app/components/auth-button.vue
+++ app/components/auth-button.vue
@@ -3,7 +3,30 @@ const authStore = useAuthStore();
 </script>
 
 <template>
+  <div v-if="!authStore.loading && authStore.user" class="dropdown dropdown-end">
+    <div
+      tabindex="0"
+      role="button"
+      class="btn m-1"
+    >
+      <div v-if="authStore.user.image" class="avatar">
+        <div class="w-8 rounded-full">
+          <img :src="authStore.user.image" :alt="authStore.user.name">
+        </div>
+      </div>
+      {{ authStore.user.name }}
+    </div>
+    <ul tabindex="0" class="menu dropdown-content bg-base-200 rounded-box z-1 w-52 p-2 shadow-sm text-base-content">
+      <li>
+        <NuxtLink to="/sign-out">
+          <Icon name="tabler:logout-2" size="24" />
+          Đăng xuất
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
   <button
+    v-else
     class="btn btn-accent"
     :disabled="authStore.loading"
     @click="authStore.signIn"
