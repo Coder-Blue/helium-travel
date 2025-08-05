000c3fdfe758bdeb4869e0d560cf64ef69d84034
Coder-Blue <trananhquan1009@gmail.com>
2025-08-06 01:06:37 +0700

    06/08/25 - create location form / preventing csrf attacks

--- app/lib/db/schema/location.ts
+++ app/lib/db/schema/location.ts
@@ -1,4 +1,6 @@
 import { int, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { createInsertSchema } from "drizzle-zod";
+import { z } from "zod";
 
 import { user } from "./auth";
 
@@ -12,4 +14,17 @@ export const location = sqliteTable("location", {
   userId: int().notNull().references(() => user.id),
   createdAt: int().notNull().$default(() => Date.now()),
   updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
+});
+
+export const InsertLocation = createInsertSchema(location, {
+  name: field => field.min(1, { message: "Tên địa điểm phái có ít nhất 1 kí tự" }).max(100, { message: "Tên địa điểm không được nhiều hơn 100 kí tự" }),
+  description: field => field.max(1000, { message: "Mô tả địa điểm không nhiều hơn 1000 kí tự" }),
+  lat: z.coerce.number().min(-90, { message: "Vĩ độ không được thấp hơn -90 độ" }).max(90, { message: "Vĩ độ không được cao hơn 90 độ" }),
+  long: z.coerce.number().min(-180, { message: "Kinh độ không được thấp hơn -180 độ" }).max(180, { message: "Kinh độ không được cao hơn 180 độ" }),
+}).omit({
+  id: true,
+  slug: true,
+  userId: true,
+  createdAt: true,
+  updatedAt: true,
 });
---
+++ app/pages/dashboard.vue
@@ -0,0 +1,59 @@
+<script lang="ts" setup>
+const isSidebarOpen = ref(true);
+
+onMounted(() => {
+  isSidebarOpen.value = localStorage.getItem("isSidebarOpen") === "true";
+});
+
+function toggleSidebar() {
+  isSidebarOpen.value = !isSidebarOpen.value;
+  localStorage.setItem("isSidebarOpen", isSidebarOpen.value.toString());
+}
+</script>
+
+<template>
+  <div class="flex flex-1">
+    <div class="bg-base-100 transition-all duration-300" :class="{ 'w-64': isSidebarOpen, 'w-16': !isSidebarOpen }">
+      <div
+        class="flex hover:cursor-pointer hover:bg-base-200 p-2"
+        :class="{ 'justify-center': !isSidebarOpen, 'justify-end': isSidebarOpen }"
+        @click="toggleSidebar"
+      >
+        <Icon
+          v-if="isSidebarOpen"
+          name="tabler:chevron-left"
+          size="32"
+        />
+        <Icon
+          v-else
+          name="tabler:chevron-right"
+          size="32"
+        />
+      </div>
+      <div class="flex flex-col gap-2">
+        <SidebarButton
+          href="/dashboard"
+          :show-label="isSidebarOpen"
+          label="Vị trí"
+          icon="tabler:map"
+        />
+        <SidebarButton
+          href="/dashboard/add"
+          :show-label="isSidebarOpen"
+          label="Thêm vị trí"
+          icon="tabler:circle-plus-filled"
+        />
+        <div class="divider" />
+        <SidebarButton
+          href="/sign-out"
+          :show-label="isSidebarOpen"
+          label="Đăng xuất"
+          icon="tabler:logout-2"
+        />
+      </div>
+    </div>
+    <div class="flex-1">
+      <NuxtPage />
+    </div>
+  </div>
+</template>
