0878c59bf0eced229f6a604b9a912604f5c4a10f
Noah Trần <trananhquan1009@gmail.com>
2025-08-05 14:56:04 +0700

    05/08/25 - add auth state / sign out | fix libsql (#56)

--- app/lib/db/index.ts
+++ app/lib/db/index.ts
@@ -1,13 +1,16 @@
+import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 
 import env from "../env";
 import * as schema from "./schema";
 
+const client = createClient({
+  url: env.TURSO_DATABASE_URL,
+  authToken: env.TURSO_AUTH_TOKEN,
+});
+
 const db = drizzle({
-  connection: {
-    url: env.TURSO_DATABASE_URL,
-    authToken: env.NODE_ENV === "development" ? undefined : env.TURSO_AUTH_TOKEN,
-  },
+  client,
   casing: "snake_case",
   schema,
 });
