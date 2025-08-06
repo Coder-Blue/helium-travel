473457712325f9764adb7303659636d2def1b304
Noah Trần <trananhquan1009@gmail.com>
2025-08-06 19:35:18 +0700

    06/08/25 - add locations to sidebar (#65)

---
+++ app/stores/sidebar.ts
@@ -0,0 +1,16 @@
+type SidebarItem = {
+  id: string;
+  label: string;
+  icon: string;
+  href: string;
+};
+
+export const useSidebarStore = defineStore("useSidebarStore", () => {
+  const sidebarItems = ref<SidebarItem[]>([]);
+  const loading = ref(false);
+
+  return {
+    loading,
+    sidebarItems,
+  };
+});
