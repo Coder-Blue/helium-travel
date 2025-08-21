7a483c09dc3cc33524d386afe94ff3b3f5c0357b
Noah Trần <trananhquan1009@gmail.com>
2025-08-21 14:57:43 +0700

    :sparkles: - show location log cards list (#76)

--- app/pages/dashboard/location/[slug].vue
+++ app/pages/dashboard/location/[slug].vue
@@ -54,7 +54,7 @@ onBeforeRouteUpdate((to) => {
 </script>
 
 <template>
-  <div class="p-4 min-h-64">
+  <div class="page-content-top">
     <div v-if="loading">
       <div class="loading" />
     </div>
@@ -113,6 +113,24 @@ onBeforeRouteUpdate((to) => {
           <Icon name="tabler:map-pin-plus" size="24" />
         </NuxtLink>
       </div>
+    </div>
+    <div v-if="route.name === 'dashboard-location-slug' && location?.locationLogs.length" class="location-list">
+      <LocationCard
+        v-for="log in location.locationLogs"
+        :key="log.id"
+        :map-point="createMapPointFromLocationLog(log)"
+      >
+        <template #top>
+          <p class="text-sm italic text-gray-500">
+            <span v-if="log.startedAt !== log.endedAt">
+              {{ formatDisplayDate(log.startedAt) }} / {{ formatDisplayDate(log.endedAt) }}
+            </span>
+            <span v-else>
+              {{ formatDisplayDate(log.startedAt) }}
+            </span>
+          </p>
+        </template>
+      </LocationCard>
     </div>
     <div v-if="route.name !== 'dashboard-location-slug'">
       <NuxtPage />
