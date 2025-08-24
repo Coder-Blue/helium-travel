09ec096a5b49bd148fc76a01a58b63c5a30d43aa
Noah Trần <trananhquan1009@gmail.com>
2025-08-24 14:45:59 +0700

    :sparkles: - setup s3 storage (#81)

--- app/lib/env.ts
+++ app/lib/env.ts
@@ -10,6 +10,15 @@ const EnvSchema = z.object({
   BETTER_AUTH_URL: z.string(),
   AUTH_GOOGLE_CLIENT_ID: z.string(),
   AUTH_GOOGLE_CLIENT_SECRET: z.string(),
+  MINIO_ROOT_USER: z.string(),
+  MINIO_ROOT_PASSWORD: z.string(),
+  MINIO_API_PORT: z.string(),
+  MINIO_CONSOLE_PORT: z.string(),
+  S3_ENDPOINT: z.string(),
+  S3_ACCESS_KEY: z.string(),
+  S3_SECRET_KEY: z.string(),
+  S3_REGION: z.string(),
+  S3_BUCKET: z.string(),
 });
 
 export type EnvSchema = z.infer<typeof EnvSchema>;
